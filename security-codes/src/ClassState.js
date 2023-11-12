40d13e328d2a6ea51874ec79d9007ce692abcf17
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-11 21:00:35 -0500

    Métodos del ciclo de vida en React.Component

--- security-codes/src/ClassState.js
+++ security-codes/src/ClassState.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Loading } from './Loading.js'
 
 class ClassState extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       error: false,
+      loading: false,
     };
+  }
+
+  // Se ejecuta antes de renderizar el componente.
+  UNSAFE_componentWillMount() {}
+
+  componentDidMount() {}
+
+  // Se ejecuta cada vez que actualicemos el estado, equivalente al useEffect.
+  componentDidUpdate() {
+    if (this.state.loading) {
+      setTimeout(() => {
+        this.setState({ loading: false });
+      }, 3000);
+    }
   }
 
   render() {
@@ -15,8 +30,9 @@ class ClassState extends React.Component {
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escribe el código de seguridad.</p>
         {this.state.error && <p>Error: El código es incorrecto.</p>}
+        {this.state.loading && <Loading />}
         <input placeholder="Código de seguridad" />
-        <button onClick={() => this.setState({ error: !this.state.error })}>
+        <button onClick={() => this.setState({ loading: true })}>
           Comprobar
         </button>
       </div>
