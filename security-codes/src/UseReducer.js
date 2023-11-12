5ef70624b86ef95e540c46a9b5898520f8b2f9c9
LAPTOP-VUQ1J6NP\Victor_Moreno <victorsahir1011@gmail.com>
2023-11-12 02:23:36 -0500

    Independizando los componentes del reducer.

--- security-codes/src/UseReducer.js
+++ security-codes/src/UseReducer.js
@@ -3,63 +3,6 @@ import React, { useReducer, useEffect } from "react";
 const SECURITY_CODE = "paradigma";
 
 function UseReducer({ name }) {
-  const initialState = {
-    value: "",
-    error: false,
-    loading: false,
-    deleted: false,
-    confirmed: false,
-  };
-
-  const actionTypes = {
-    confirm: "CONFIRM",
-    error: "ERROR",
-    write: "WRITE",
-    check: "CHECK",
-    delete: "DELETE",
-    reset: "RESET",
-  };
-
-  const reducerObject = (state, payload) => ({
-    [actionTypes.error]: {
-      ...state,
-      error: true,
-      loading: false,
-      confirmed: false,
-    },
-    [actionTypes.confirm]: {
-      ...state,
-      error: false,
-      loading: false,
-      confirmed: true,
-    },
-    [actionTypes.write]: {
-      ...state,
-      value: payload,
-    },
-    [actionTypes.check]: {
-      ...state,
-      loading: true,
-    },
-    [actionTypes.delete]: {
-      ...state,
-      deleted: true,
-    },
-    [actionTypes.reset]: {
-      ...state,
-      confirmed: false,
-      deleted: false,
-      value: "",
-    },
-  });
-
-  const reducer = (state, action) => {
-    if (reducerObject(state)[action.type]) {
-      return reducerObject(state, action.payload)[action.type];
-    } else {
-      return state;
-    }
-  };
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -144,5 +87,63 @@ function UseReducer({ name }) {
     );
   }
 }
+
+const initialState = {
+  value: "",
+  error: false,
+  loading: false,
+  deleted: false,
+  confirmed: false,
+};
+
+const actionTypes = {
+  confirm: "CONFIRM",
+  error: "ERROR",
+  write: "WRITE",
+  check: "CHECK",
+  delete: "DELETE",
+  reset: "RESET",
+};
+
+const reducerObject = (state, payload) => ({
+  [actionTypes.error]: {
+    ...state,
+    error: true,
+    loading: false,
+    confirmed: false,
+  },
+  [actionTypes.confirm]: {
+    ...state,
+    error: false,
+    loading: false,
+    confirmed: true,
+  },
+  [actionTypes.write]: {
+    ...state,
+    value: payload,
+  },
+  [actionTypes.check]: {
+    ...state,
+    loading: true,
+  },
+  [actionTypes.delete]: {
+    ...state,
+    deleted: true,
+  },
+  [actionTypes.reset]: {
+    ...state,
+    confirmed: false,
+    deleted: false,
+    value: "",
+  },
+});
+
+const reducer = (state, action) => {
+  if (reducerObject(state)[action.type]) {
+    return reducerObject(state, action.payload)[action.type];
+  } else {
+    return state;
+  }
+};
 
 export { UseReducer };
